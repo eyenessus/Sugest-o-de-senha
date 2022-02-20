f79d6d8c611bd3da8a6eb66a5d8c8a2eea16d608
Eyenessus <eyenessus@gmail.com>
2022-02-20 15:01:21 -0300

    Programa que sugere uma senha pela idade em C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Esforço
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int dia,mes,ano,idade;
+            Console.Clear();
+            Console.WriteLine("Qual seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Qual o dia do seu nascimento? ");
+            dia = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qual o mes do seu nascimento? ");
+            mes = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qual o ano do seu nascimento? ");
+            ano = Convert.ToInt16(Console.ReadLine());
+            idade = 2021 - ano;
+            if (idade <= 18)
+             {
+                Console.WriteLine("Uma sugestão de senha para você e: "+ nome+idade);
+            }
+            else
+            {
+                Console.WriteLine("Uma sugestão de senha para você e: " + idade+nome);
+            }
+        }
+    }
+}
